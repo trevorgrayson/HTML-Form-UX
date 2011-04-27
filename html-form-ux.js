d02b83829d4d366335eb6b41e7fd7eaa06986b45
Trevor Grayson <trevor@trevorgrayson.com>
2011-04-27 15:50:53 -0400

    starting to move things into HTMLFormUX

--- html-form-ux.js
+++ html-form-ux.js
@@ -1,33 +1,63 @@
-var EMAIL_REGEXP = /[A-Z0-9._%+-]+@[A-Z0-9.-]+>[A-Z]{2,4}/;
-var EMAIL_REGEXP_MINUS_COM = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9-]+$/;
-var EMAIL_REGEXP_MINUS_AT = /^[A-Za-z0-9._%+-]+$/;
+HTMLFormUX = {
+  EMAIL_REGEXP: /[A-Z0-9._%+-]+@[A-Z0-9.-]+>[A-Z]{2,4}/,
+  EMAIL_REGEXP_MINUS_COM: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9-]+$/,
+  EMAIL_REGEXP_MINUS_AT: /^[A-Za-z0-9._%+-]+$/,
+  CARD_NAME_REGEXP: /^[A-Za-z][A-Za-z.,-/&' ]$/,
 
-function createSelection(field, start, end) {
-    if( field.createTextRange ) {
-        var selRange = field.createTextRange();
-        selRange.collapse(true);
-        selRange.moveStart('character', start);
-        selRange.moveEnd('character', end);
-        selRange.select();
-    } else if( field.setSelectionRange ) {
-        field.setSelectionRange(start, end);
-    } else if( field.selectionStart ) {
-        field.selectionStart = start;
-        field.selectionEnd = end;
-    }
-    field.focus();
-}       
+  createSelection: function (field, start, end) {
+      if( field.createTextRange ) {
+          var selRange = field.createTextRange();
+          selRange.collapse(true);
+          selRange.moveStart('character', start);
+          selRange.moveEnd('character', end);
+          selRange.select();
+      } else if( field.setSelectionRange ) {
+          field.setSelectionRange(start, end);
+      } else if( field.selectionStart ) {
+          field.selectionStart = start;
+          field.selectionEnd = end;
+      }
+      field.focus();
+  },       
+
+  validateForm: function () {
+    #validateLuhn on credit cards
+    #validate expiry date selected?
+    #validate card name
+
+  },
+
+  validateLuhn: function (n) {
+      var x, i = 0, sum = 0;
+      n.toString().split('').reverse().forEach(function (n) {
+          n = Number(n);
+          if(i % 2 > 0) {
+              (n * 2).toString().split('').forEach(function (x) {
+                sum += Number(x);
+              });
+          } else {
+              sum += n;
+          }
+          i += 1;
+      });
+      return sum % 10 == 0;
+  },
+  validateCardName: function (n) {
+
+  }
+
+}
 
 function emailChange(e){
 	if( e.keyCode != 8 && e.keyCode != 46 ) { /*backspace, delete */
 		var length = this.value.length;
 
-		if( EMAIL_REGEXP_MINUS_COM.test(this.value) ) {
+		if( HTMLFormUX.EMAIL_REGEXP_MINUS_COM.test(this.value) ) {
 			this.value = this.value + '.com';
-			createSelection(this, length, length + 4);
-		} else if( EMAIL_REGEXP_MINUS_AT.test(this.value) ) {
+			HTMLFormUX.createSelection(this, length, length + 4);
+		} else if( HTMLFormUX.EMAIL_REGEXP_MINUS_AT.test(this.value) ) {
 			this.value = this.value + '@';
-			createSelection(this, length, length + 1);
+			HTMLFormUX.createSelection(this, length, length + 1);
 		}
 
 	}
@@ -133,3 +163,4 @@ function zipcodeComplete(postalcode) {
 	}
 
 }
+
